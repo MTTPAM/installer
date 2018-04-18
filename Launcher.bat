24a10ba5acc50f17d84ad6fabbac688461004563
linktlh <lewistlh@gmail.com>
2018-04-17 20:36:07 -0500

    my bad

--- Launcher.bat
+++ Launcher.bat
@@ -8,12 +8,9 @@ goto Y)
 start README.txt
 set didread="n" 
 SET /P didread="did you read README: (Y or N)"
-echo %didread%
-echo is "%didread%"
 IF /I "%didread%"=="y" goto Y
 goto A
 
 :Y
-echo hi
 %PPYTHON_PATH% launcher.py
 pause
