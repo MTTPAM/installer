eaa848bbfd3857f1cce9121983d7e7f330120545
linktlh <linktlh@gmail.com>
2018-04-21 23:44:27 -0500

    yea so giy

--- dontrunme.bat
+++ dontrunme.bat
@@ -1,6 +1,6 @@
-set git=git/bin/git
-%git% init .
-%git% remote add origin https://github.com/MTTPAM/PublicRelease.git
-%git% fetch --all
-%git% reset --hard origin/master
+
+git init .
+git remote add origin https://github.com/MTTPAM/PublicRelease.git
+git fetch --all
+git reset --hard origin/master
 del dontrunme.bat
