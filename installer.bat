a6ae2b893de770a605b0ee4c3eda48d92b304872
linktlh <linktlh@gmail.com>
2018-11-12 21:27:43 -0600

    Setup Better setup
    This should make things easier for end user by removing possible failure points.

--- installer.bat
+++ installer.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 set /P PPYTHON_PATH=<PPYTHON_PATH
+cls
 %PPYTHON_PATH% installer.py
 pause
--- installer.py
+++ installer.py
@@ -1,50 +1,35 @@
-####################################
-###    MTTPA's Git Installer     ###
-###    Development Launcher      ###
-###   For  Testing  uses  only   ###
-####################################
+#################################
+#    MTTPAM's Git Installer     #
+#     Development Launcher      #
+#   For  Testing  uses  only    #
+#################################
 
-#I see your Curious if this is a virus or something
-#Well I'll describe whats going on here in detail.
-
-#First we bring in some things already built into python
-import os, sys
-#we then tell the cmd to clear
-os.system('cls')
-#we ask the system do either one of these file paths exist? If not then:
-if (os.path.exists("C:/Program Files (x86)/Git/bin") == 0 and os.path.exists("C:/Program Files/Git/bin") == 0):
-     #set the batch file color to red
-     os.system("Color 04")
-     #Print out these messages to the cmd for the user to read
-     print("Git Is not installed! please install and run again\n")
-     print("Git MUST be installed to the default directory")
-     print("https://git-scm.com/download/win")
-     #Exit Don't Keep Going.
-     sys.exit()  
-#Does This file exist? If not then:
-if (os.path.isfile('dontrunme.bat') == 0):
-        #Print out to cmd
-		print("Checking for updates . . .")
-        #Set Color to green 
-		os.system("color 02")
-		#Fetch the github repo for changes
-		os.system("git fetch")
-		#Force any modified files to be redownloaded/update
-		os.system("git reset --hard origin/master")
-#does this file exist? If It DOES then:
-if (os.path.isfile('dontrunme.bat') == 1):
-        #set Color To Gold
-        os.system("color 06")
-		#Print Out to cmd:
-        print("running first time setup . . .")
-        print("This will take a while.")
-		#Run Batch File
-        os.system('dontrunme.bat')	
-#Set Color to Cyan
-os.system("color 09")
-#Print out to CMD
-print("Done; Launching launcher")
-#Launch the launcher this should have downloaded.
-os.system("start launcher.bat")
-#Exit
+import os
+import sys
+dev = False
+if os.path.exists("C:/Program Files (x86)/Git/bin") == 0 and os.path.exists("C:/Program Files/Git/bin") == 0:
+    os.system("Color 04")
+    print("Git Is not installed! please install and run again\n")
+    print("Git MUST be installed to the default directory")
+    print("https://git-scm.com/download/win")
+    sys.exit()
+else:
+    os.system("color 06")
+    print("running first time setup . . .")
+    print("This will take a while.")
+    if dev:
+        print("intitalizing Git")
+    os.system("git init .")
+    if dev:
+        print("adding remote repo")
+    os.system("git remote add origin https://github.com/MTTPAM/PublicRelease.git")
+    if dev:
+        print("Fetching files, and updating.")
+    os.system("git fetch --all")
+    os.system("git reset --hard")
+    os.system("color 09")
+    print("Done; Launching launcher")
+    os.system("del installer.py /A")
+    os.system("del installer.bat /A")
+    os.system("start launcher.bat")
 os.exit()
